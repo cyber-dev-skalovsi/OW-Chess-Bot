697aaff4f629cf7834950e0d72e366f0abc4ede1
cyber-dev-skalovsi <172505522+cyber-dev-skalovsi@users.noreply.github.com>
2025-10-24 23:50:05 +0200

    Simple Chess Color grid added.

--- Chess Bot/MainWindow.xaml.cs
+++ Chess Bot/MainWindow.xaml.cs
@@ -1,28 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Chess_Bot
+namespace ChessBot 
 {
-    /// <summary>
-    /// Interaktionslogik für MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window  // "partial" ist entscheidend!
     {
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent();  // Das hier muss vor der Nutzung von ChessBoard stehen!
+            DrawBoard();
+        }
+
+        private void DrawBoard()
+        {
+            if (ChessBoard == null)
+            {
+                MessageBox.Show("ChessBoard ist null – XAML nicht geladen!");
+                return;
+            }
+
+            for (int row = 0; row < 8; row++)
+            {
+                for (int col = 0; col < 8; col++)
+                {
+                    var square = new Rectangle  // Besser als Image für einfache Quadrate
+                    {
+                        Width = 75,
+                        Height = 75,
+                        Fill = new SolidColorBrush((row + col) % 2 == 0 ?
+                            Colors.LightGray : Colors.DarkGray)  // Alternierende Farben
+                    };
+                    ChessBoard.Children.Add(square);
+                }
+            }
         }
     }
 }
